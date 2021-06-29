9a084ad091743ed50d8737787d65eb924bd2145d
SunWoo-An <84854953+SunWoo-An@users.noreply.github.com>
2021-06-30 07:48:11 +0900

    Update RandomGatcha.java

--- RandomGatcha.java
+++ RandomGatcha.java
@@ -3,12 +3,20 @@ import java.util.ArrayList;
 public class RandomGatcha {
 	ArrayList<Person> list = new ArrayList<Person>();
 	
-	public RandomGatcha(ArrayList<Person> list) {
+	private int num = 0; // 사물함 번호
+	int size = 0; // 몇번
+	
+	public RandomGatcha(ArrayList<Person> list,int size) {
 		this.list = list;
+		this.size = size;
 	}
 	
 	public void randomgatcha() {
-		
+		int winning = (int)(Math.random()% size + 1);
+			
 	}
 	
+	public void Excellent() {
+		
+	}
 }
